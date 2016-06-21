721d6870114ed99bd408aa53982c681bc36d8267
Fynn Schröder <zody22@gmail.com>
2016-06-21 23:10:28 +0200

    made GA + controller use generics, switch fitness from int to double

--- src/main/java/SimulationRoundResult.java
+++ src/main/java/SimulationRoundResult.java
@@ -1,10 +1,10 @@
 
 public final class SimulationRoundResult {
-	public int maxFitness;
-	public int minFitness;
-	public int avgFitness;
+	public double maxFitness;
+	public double minFitness;
+	public double avgFitness;
 
-	public SimulationRoundResult(int maxFitness, int minFitness, int avgFitness) {
+	public SimulationRoundResult(double maxFitness, double minFitness, double avgFitness) {
 		this.maxFitness = maxFitness;
 		this.minFitness = minFitness;
 		this.avgFitness = avgFitness;
